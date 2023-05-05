e4578de0bf54d27f0119e674411c6059c7f3b2be
aniket_chachane <aniketcha7@gmail.com>
2023-05-05 15:04:48 +0530

    new report submitted

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import stringType1 from './stringt/stringt';
+import ObjT from './Objt/objt';
+import NumberT from './Other/numbert';
 
 function App() {
+  console.log("using usestate")
 
-  const [details, setdetails] = useState("Aniket")
-  function updatedetails() 
-  {
-    setdetails("chachane")  
-  }
-  
-  return (
-    <div className="App">
-     {details}
-     <br/>
-     <br/>
-     <button onClick={updatedetails}>
-      Tap To See My Sur Name =
-     </button>
+return(
+ <div className ="App">
+
+     <h4>****String usestate****</h4>
+     {stringType1()}
+
+     <hr/><h4>****Object Usestate****</h4>
+     {ObjT()}
+
+     <hr/>
+     <h4>****Number Usestate****</h4>
+     {NumberT()}
 
     </div>
-  )
-}
 
+)
+}
 
 export default App;
